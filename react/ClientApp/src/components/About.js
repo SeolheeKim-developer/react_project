edb7bb566d00db41f7ecccb6f15bf735b49c5e0a
Seolhee <seolhee@campigotto.com>
2023-05-12 12:57:28 -0400

    add more subcomponents into About, Contact components

--- react/ClientApp/src/components/About.js
+++ react/ClientApp/src/components/About.js
@@ -1,7 +1,43 @@
 ﻿import React, { Component } from 'react';
+import { TechList } from './Shared/TechList/TechList';
+import { SiteList } from './Shared/SiteList/SiteList';
 
 export class About extends Component {
     render() {
-        return (<div><h1>imformation</h1></div>);
+        const title = "information";
+        const message = "iHello there. this is ReactProject site.";
+        return (
+        <div>
+            <h1>{title}</h1>
+            <h3>{message}</h3>
+
+            <p>This is a react project by Seolhee</p>
+
+            <div className='row mb-5'>
+                <div className='col-md-6'>
+                    <div className='thumbnail'>
+                        <img className="img-fluid" src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaG5vbG9neXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" alt="computer screen"/>
+                        <i>Photo by https://unsplash.com</i>
+                    </div>
+                </div>
+                <div className='col-md-6'>
+                    <div className='thumbnail'>
+                        <img className="img-fluid" src="https://images.unsplash.com/photo-1496171367470-9ed9a91ea931?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHRlY2hub2xvZ3l8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60" alt="computer on the desk" />
+                        <i>Photo by https://unsplash.com</i>
+                    </div>
+                </div>
+            </div>
+
+            <div className='row mb-5'>
+                <div class="col-md-6">
+                    <h3><i className="fa fa-wrench"></i>Stacks is used in Currrent site </h3>
+                    <TechList></TechList>
+                </div>
+                <div className="col-md-6">
+                    <h3><i className="fa fa-sitemap"></i>recommended websites </h3>
+                    <SiteList></SiteList>
+                </div>
+            </div>
+        </div>);
     }
 }
--- react/ClientApp/src/components/Contact.js
+++ react/ClientApp/src/components/Contact.js
@@ -1,14 +1,40 @@
 import React, { Component } from "react";
 import { ContactDetails } from "./ContactDetails/ContactDetails";
+import { TechList } from "./Shared/TechList/TechList";
+import { SiteList } from "./Shared/SiteList/SiteList";
 
 export class Contact extends Component {
     render() {
+        const title = "Contact information";
+        const message = "Hello there. this is ReactProject site.";
         return(
-            <div>
-                <h1>Contact</h1>
-                <p>contacts here...</p>
+            <>
+                <h1>{title}</h1>
+                <h3>{message}</h3>
+
+                <p>This is a react project by Seolhee</p>
+
                 <ContactDetails ></ContactDetails>
+
+                <div className="alert alert-warning alert-dismissible mb-5">
+                This is an alert message.
+                    <button type="button" className="close" data-bs-dismiss="alert" aria-hidden="true">
+                        x
+                    </button>
+                    
+                </div>
+
+                <div className='row mb-5'>
+                <div class="col-md-6">
+                    <h3><i className="fa fa-wrench"></i>Stacks is used in Currrent site </h3>
+                    <TechList></TechList>
+                </div>
+                <div className="col-md-6">
+                    <h3><i className="fa fa-sitemap"></i>recommended websites </h3>
+                    <SiteList></SiteList>
+                </div>
             </div>
+            </>
             
         );
     }
